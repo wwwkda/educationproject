0f495e07de8bcf3e30540f3a985f61281728c147
wwwkda <wwwwkda@mail.ru>
2021-02-08 20:27:14 +0300

    подготовка к медиа запросам

--- Фрилансер по жизни/javascript/js-syntax2/js/script.js
+++ Фрилансер по жизни/javascript/js-syntax2/js/script.js
@@ -9,19 +9,19 @@
 "use strict";
 console.log(Я учу JS);
 
-Вариант №1
-console.log('Учим JS')
+// Вариант №1
+// console.log('Учим JS')
 
-Вариант №2
-console.log('JS')
-console.log('Учим')
+// Вариант №2
+// console.log('JS')
+// console.log('Учим')
 
-Вариант №3
-console.log('Учим') console.log('JS')
+// Вариант №3
+// console.log('Учим') console.log('JS')
 
-Вариант №4
-console.log('Учим);
-console.log('JS');
+// Вариант №4
+// console.log('Учим);
+// console.log('JS');
 
 
 //Ну и конечно же, учи теорию.
